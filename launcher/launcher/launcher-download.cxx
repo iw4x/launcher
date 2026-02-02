ef1b129ba97b4f565039c0da8f38a81b68819207
William Roy <wroy@proton.me>
2026-02-02 14:11:29 -0500

    Tidy up download coordinator implementation details

--- launcher/launcher/launcher-download.cxx
+++ launcher/launcher/launcher-download.cxx
@@ -1,33 +1,33 @@
 #include <launcher/launcher-download.hxx>
 
-#include <launcher/download/download-manager.hxx>
-#include <launcher/download/download-task.hxx>
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+#include <stdexcept>
+#include <vector>
 
 #include <openssl/evp.h>
 
-#include <fstream>
-#include <sstream>
-#include <iomanip>
-#include <stdexcept>
-#include <vector>
+#include <launcher/download/download-manager.hxx>
+#include <launcher/download/download-task.hxx>
 
 using namespace std;
 
 namespace launcher
 {
+  // Note: we default to serial execution (1) unless told otherwise.
+  //
   download_coordinator::
-  download_coordinator (asio::io_context& ioc)
-    : ioc_ (ioc),
-      manager_ (make_unique<manager_type> (ioc, 1)),
-      http_ (make_unique<http_client> (ioc))
+  download_coordinator (asio::io_context& c)
+    : download_coordinator (c, 1)
   {
   }
 
   download_coordinator::
-  download_coordinator (asio::io_context& ioc, size_t mp)
-    : ioc_ (ioc),
-      manager_ (make_unique<manager_type> (ioc, mp)),
-      http_ (make_unique<http_client> (ioc))
+  download_coordinator (asio::io_context& c, size_t n)
+    : ioc_ (c),
+      manager_ (make_unique<manager_type> (c, n)),
+      http_ (make_unique<http_client> (c))
   {
   }
 
@@ -65,42 +65,36 @@ namespace launcher
   }
 
   shared_ptr<download_coordinator::task_type> download_coordinator::
-  queue_download (string url, fs::path target)
+  queue_download (string u, fs::path f)
   {
-    request_type r;
-    r.urls.push_back (move (url));
-    r.target = move (target);
-
-    // Default the task name to the filename. This is usually what we want
-    // for UI reporting unless the caller overrides it.
+    // Synthesize the request.
     //
-    r.name = r.target.filename ().string ();
+    // Note that we infer the display name from the filename so the UI has
+    // something reasonable to show without the caller having to be explicit.
+    //
+    request_type r;
+    r.urls.push_back (move (u));
+    r.name = f.filename ().string ();
+    r.target = move (f);
 
     return queue_download (move (r));
   }
 
   shared_ptr<download_coordinator::task_type> download_coordinator::
-  queue_download (string url,
-                  fs::path target,
-                  download_verification method,
-                  string value)
+  queue_download (string u, fs::path f, download_verification m, string v)
   {
     request_type r;
-    r.urls.push_back (move (url));
-    r.target = move (target);
+    r.urls.push_back (move (u));
+    r.target = move (f);
     r.name = r.target.filename ().string ();
 
-    r.verification_method = method;
-    r.verification_value = move (value);
+    r.verification_method = m;
+    r.verification_value = move (v);
 
     return queue_download (move (r));
   }
 
   // Statistics & State.
-  //
-  // Most of these are just pass-throughs to the manager, but they allow us
-  // to keep the manager implementation details hidden from the coordinator's
-  // clients.
   //
 
   size_t download_coordinator::
@@ -157,15 +151,13 @@ namespace launcher
   asio::awaitable<void> download_coordinator::
   execute_all ()
   {
-    // Wait for the manager to drain the queue.
+    // Block here until the manager drains the queue.
     //
     co_await manager_->download_all ();
 
-    // @@ TODO: Should we collect the specific errors here and throw a
-    // composite exception? For now, we assume the caller checks the
-    // task states or the failed count.
+    // @@ TODO: We might want to aggregate errors into a composite exception
+    // later if we find that checking individual tasks is too cumbersome.
     //
-
     co_return;
   }
 
@@ -174,6 +166,9 @@ namespace launcher
   {
     manager_->clear ();
   }
+
+  // Accessors.
+  //
 
   download_coordinator::manager_type& download_coordinator::
   manager () noexcept
